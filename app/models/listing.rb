85dbad014d855efbb5ab699ff4c3e782f81bac80
Tith Panha <tith.phanha@yahoo.com>
2015-08-23 17:48:13 +0700

    Add validation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -9,4 +9,7 @@ class Listing < ActiveRecord::Base
                       :dropbox_credentials => Rails.root.join("config/dropbox.yml")
     validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
   end
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates_attachment_presence :image
 end
