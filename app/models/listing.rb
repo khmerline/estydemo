4268ae62a51072d1ca045492348ee16054484010
Tith Panha <tith.phanha@yahoo.com>
2015-08-23 17:01:45 +0700

    add devise

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,7 @@ class Listing < ActiveRecord::Base
 
   if Rails.env.development?
     has_attached_file :image, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "default.png"
+    validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
   else
     has_attached_file :image, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "default.png",
                       :storage => :dropbox,
