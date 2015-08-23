1f8651d7339cef8def6f5fe89031a322e41d7720
Tith Panha <tith.phanha@yahoo.com>
2015-08-23 23:09:14 +0700

    fixed error

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,10 +1,10 @@
 class Listing < ActiveRecord::Base
 
   if Rails.env.development?
-    has_attached_file :image, styles: { medium: "300x200>", thumb: "100x100>" }, default_url: "default.png"
+    has_attached_file :image, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "default.png"
     validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
   else
-    has_attached_file :image, styles: { medium: "300x200>", thumb: "100x100>" }, default_url: "default.png",
+    has_attached_file :image, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "default.png",
                       :storage => :dropbox,
                       :dropbox_credentials => Rails.root.join("config/dropbox.yml")
     validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
